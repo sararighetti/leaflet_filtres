e2a24774feecf5936a2bc90a18b3fe78bc065cb6
sararighetti <71082145+sararighetti@users.noreply.github.com>
2021-04-13 15:36:58 +0200

    Update _db.php

--- api/_db.php
+++ api/_db.php
@@ -1,6 +1,14 @@
 <?php
 
 $mysqli = new mysqli('127.0.0.1', 'root', '', 'my_app');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
 
 if ($mysqli->connect_errno) {
     echo "Lo sentimos, este sitio web está experimentando problemas.";
